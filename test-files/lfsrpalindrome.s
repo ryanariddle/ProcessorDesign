a09a08a0f20ecd45a4e011c4feed3254fef609fa
ryanriddle@berkeley.edu <ryanriddle@berkeley.edu>
2015-03-20 14:53:59 -0700

    SPRING BREAKgit add test-files/lfsrpalindrome.s git add test-files/lfsrpalindrome.s  (in Soda)

---
+++ test-files/lfsrpalindrome.s
@@ -0,0 +1,18 @@
+LfsrPalindrome:
+	add $t0, $a0, $0
+	add $t1, $a0, $0
+	addiu $t2, $0, 1
+	add $v0, $a0, $0
+
+loop:
+	lfsr $t1, $t1
+	bitpal $t3, $t1
+	beq $t2, $t3, palindrone
+	beq $t1, $t0, end
+
+palindrone:
+	add $v0, $t1, $0
+	j loop
+
+end:
+	jr $ra
